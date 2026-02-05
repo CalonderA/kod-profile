f8e15c34ee5ec4429d88e66dc8c90379b4c1568e
Calonder <androsenkoaleksandr6@gmail.com>
2026-02-05 20:21:02 +0300

    Практика 05.02.26

---
+++ course-1/practices/praktuka_05_02/Class1.cs
@@ -0,0 +1,28 @@
+﻿public class Library
+{
+    public string Name { get; set; }
+
+    public List<Book> Books { get; set; }
+}
+
+var library = new Library
+{
+    Name = "Городская библиотека",
+    Books = new List<Book>
+    {
+        new Book { Title = "Война и мир", Author = "Толстой", Year = 1869 },
+        new Book { Title = "Преступление и наказание", Author = "Достоевский", Year = 1866 }
+    }
+};
+var options = new JsonSerializerOptions { WriteIndented = true };
+string json = JsonSerializer.Serialize(library, options);
+File.WriteAllText("library.json", json);
+
+string jsonFromFile = File.ReadAllText("library.json");
+var libraryFromFile = JsonSerializer.Deserialize<Library>(jsonFromFile);
+
+Console.WriteLine($"Библиотека: {libraryFromFile.Name}");
+foreach (var b in libraryFromFile.Books)
+{
+    Console.WriteLine($"Книга: \"{b.Title}\", автор: {b.Author}, год: {b.Year}");
+}
---
+++ course-1/practices/praktuka_05_02/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Text.Json.Serialization;
+
+var books = new List<Book>
+{
+   new Book {Title = "Война и мир", Author = "Толстой", Year = 1869},
+    new Book { Title = "Преступление и наказание", Author = "Достоевский", Year = 1866 }
+
+};
+
+
+string Json = JsonSerializer.Serialize(books, new JsonSerializerOptions { WriteIndented = true });
+File.WriteAllText("books.json", Json);
+
+
+
+
+string jsonFromFile = File.ReadAllText("books.json");
+var booksFromFile = JsonSerializer.Deserialize<List<Book>>(jsonFromFile);
+
+foreach (var book in booksFromFile)
+{
+    Console.WriteLine("$Название: {book.Title}, Автор: {book.Author}, Год: {book.Year}");
+
+}
+
+var optrions = new JsonSerializerOptions { WriteIndented = true };
+string json = JsonSerializer.Serialize(books, optrions);
+File.WriteAllText("books.json" , json);
+
+public class Book
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+
+    [JsonPropertyName("author")]
+    public string Author { get; set; }
+
+    [JsonPropertyName("publication_year")]
+    public int Year { get; set; }
+
+  
+
+
+
+}
+
+
